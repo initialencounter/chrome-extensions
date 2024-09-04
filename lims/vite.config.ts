ba749ff42aeee4a46b35ddf5763a7d2d2480c9a3
initialencounter <2911583893@qq.com>
2024-09-04 17:14:47 +0800

    fix: build
    Signed-off-by: initialencounter <2911583893@qq.com>

--- lims/vite.config.ts
+++ lims/vite.config.ts
@@ -11,7 +11,6 @@ export default defineConfig({
         utils: resolve(__dirname, 'src/content-script/utils.ts'),
         query: resolve(__dirname, 'src/content-script/query.ts'),
         hotkey: resolve(__dirname, 'src/content-script/hotkey.ts'),
-        pek2sek: resolve(__dirname, 'src/content-script/pek2sek.ts'),
         background: resolve(__dirname, 'src/background/index.ts'),
         options: resolve(__dirname, 'src/options/index.html'),
         popup: resolve(__dirname, 'src/popup/index.html'),
