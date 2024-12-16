46b5f378ac4517fadc1b01327758f2fbdd75a233
initialencounter <2911583893@qq.com>
2024-12-16 17:45:26 +0800

    chore
    Signed-off-by: initialencounter <2911583893@qq.com>

--- ts/src/pek/dropStackTest.ts
+++ ts/src/pek/dropStackTest.ts
@@ -10,40 +10,42 @@ import { CheckResult, PkgInfoSubType } from "../shared/types/index"
  */
 export function dropStackTest(pkgInfoSubType: PkgInfoSubType, stackTest: boolean, dropTest: boolean, stackTestEvaluation: boolean): CheckResult[] {
   let result: CheckResult[] = []
-  // 需要堆码或堆码评估单的包装
-  if (!stackTest && !stackTestEvaluation) {
-    if (pkgInfoSubType === '967, I' || pkgInfoSubType === '970, I' || pkgInfoSubType === '967, II' || pkgInfoSubType === '970, II') {
-      result.push({ ok: false, result: '967/970 未勾选堆码，或堆码评估，如果是24年报告请忽略' })
-    }
-    if (pkgInfoSubType === '966, II' || pkgInfoSubType === '969, II') {
-      result.push({ ok: false, result: '966/969 第II部分未勾选堆码，或堆码评估，如果是24年报告请忽略' })
-    }
-  }
+
   if (stackTest && stackTestEvaluation) {
     result.push({ ok: false, result: '重复勾选堆码和堆码评估' })
   }
+
   // 不需要堆码的包装
   if (['965, IA', '966, I', '968, IA', '969, I'].includes(pkgInfoSubType)) {
-    if (stackTest) {
+    if (stackTest || stackTestEvaluation) {
       result.push({ ok: false, result: `${pkgInfoSubType}不应勾选堆码` })
     }
-  }
-  // 要堆码的包装
-  if (pkgInfoSubType === '965, IB' || pkgInfoSubType === '968, IB') {
-    if (!stackTest) {
-      result.push({ ok: false, result: `${pkgInfoSubType}未勾选堆码` })
+  } else {
+    // 需要堆码或堆码评估单的包装
+    if (['967, I', '970, I', '967, II', '970, II', '966, II', '969, II'].includes(pkgInfoSubType)) {
+      if (!stackTest && !stackTestEvaluation) {
+        result.push({ ok: false, result: `${pkgInfoSubType} 未勾选堆码或堆码评估，如果是24年报告请忽略` })
+      }
+    }
+    // 只能勾选堆码，不能勾选堆码评估
+    if (pkgInfoSubType === '965, IB' || pkgInfoSubType === '968, IB') {
+      if (!stackTest) {
+        result.push({ ok: false, result: `${pkgInfoSubType}未勾选堆码` })
+      }
+      if (stackTestEvaluation) {
+        result.push({ ok: false, result: `${pkgInfoSubType}不应勾选堆码评估` })
+      }
     }
   }
-  // 不需要跌落的包装
-  if (['965, IA', '966, I', '967, I', '967, II', '968, IA', '969, I', '970, I', '970, II'].includes(pkgInfoSubType)) {
-    if (dropTest) {
-      result.push({ ok: false, result: `${pkgInfoSubType}不应勾选跌落` })
-    }
-  }
+
   // 要跌落的包装
   if (['965, IB', '968, IB', '966, II', '969, II'].includes(pkgInfoSubType)) {
     if (dropTest) {
       result.push({ ok: false, result: `${pkgInfoSubType}未勾选跌落` })
+    }
+  } else {
+    if (!dropTest) {
+      result.push({ ok: false, result: `${pkgInfoSubType}不应勾选跌落` })
     }
   }
   return result
