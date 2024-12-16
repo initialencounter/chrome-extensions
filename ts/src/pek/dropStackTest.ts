9731dfb6362e45bc729f2e96c07aec13949825c4
initialencounter <2911583893@qq.com>
2024-12-16 17:24:32 +0800

    fix: pek drop stack

--- ts/src/pek/dropStackTest.ts
+++ ts/src/pek/dropStackTest.ts
@@ -10,7 +10,7 @@ import { CheckResult, PkgInfoSubType } from "../shared/types/index"
  */
 export function dropStackTest(pkgInfoSubType: PkgInfoSubType, stackTest: boolean, dropTest: boolean, stackTestEvaluation: boolean): CheckResult[] {
   let result: CheckResult[] = []
-  // 跌落和堆码检测
+  // 需要堆码或堆码评估单的包装
   if (!stackTest && !stackTestEvaluation) {
     if (pkgInfoSubType === '967, I' || pkgInfoSubType === '970, I' || pkgInfoSubType === '967, II' || pkgInfoSubType === '970, II') {
       result.push({ ok: false, result: '967/970 未勾选堆码，或堆码评估，如果是24年报告请忽略' })
@@ -22,29 +22,29 @@ export function dropStackTest(pkgInfoSubType: PkgInfoSubType, stackTest: boolean
   if (stackTest && stackTestEvaluation) {
     result.push({ ok: false, result: '重复勾选堆码和堆码评估' })
   }
-  if (pkgInfoSubType === '965, IB') {
-    if (!stackTest) {
-      result.push({ ok: false, result: '965，IB未勾选堆码' })
-    }
-    if (!dropTest) {
-      result.push({ ok: false, result: '965，IB未勾选跌落' })
-    }
-  }
-  if (pkgInfoSubType === '965, IA' || pkgInfoSubType === '966, I' || pkgInfoSubType === '969, I') {
-    if (dropTest) {
-      result.push({ ok: false, result: '965，IA不应勾选跌落' })
-    }
+  // 不需要堆码的包装
+  if (['965, IA', '966, I', '968, IA', '969, I'].includes(pkgInfoSubType)) {
     if (stackTest) {
-      result.push({ ok: false, result: '965，IA不应勾选堆码' })
+      result.push({ ok: false, result: `${pkgInfoSubType}不应勾选堆码` })
     }
   }
-  if (pkgInfoSubType === '967, II' || pkgInfoSubType === '969, II') {
+  // 要堆码的包装
+  if (pkgInfoSubType === '965, IB' || pkgInfoSubType === '968, IB') {
+    if (!stackTest) {
+      result.push({ ok: false, result: `${pkgInfoSubType}未勾选堆码` })
+    }
+  }
+  // 不需要跌落的包装
+  if (['965, IA', '966, I', '967, I', '967, II', '968, IA', '969, I', '970, I', '970, II'].includes(pkgInfoSubType)) {
     if (dropTest) {
-      result.push({ ok: false, result: '967，II不应勾选跌落' })
+      result.push({ ok: false, result: `${pkgInfoSubType}不应勾选跌落` })
     }
   }
-  if ((pkgInfoSubType === '966, II') && !dropTest) {
-    result.push({ ok: false, result: '966，II未勾选跌落' })
+  // 要跌落的包装
+  if (['965, IB', '968, IB', '966, II', '969, II'].includes(pkgInfoSubType)) {
+    if (dropTest) {
+      result.push({ ok: false, result: `${pkgInfoSubType}未勾选跌落` })
+    }
   }
   return result
 }
