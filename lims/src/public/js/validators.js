2830ce86128f6c7a9da56b4e4be28ac452db32a3
initialencounter <2911583893@qq.com>
2024-12-16 17:46:51 +0800

    sync: 46b5f37

--- lims/src/public/js/validators.js
+++ lims/src/public/js/validators.js
@@ -384,40 +384,36 @@
   // src/pek/dropStackTest.ts
   function dropStackTest(pkgInfoSubType, stackTest, dropTest, stackTestEvaluation) {
     let result = [];
-    if (!stackTest && !stackTestEvaluation) {
-      if (pkgInfoSubType === "967, I" || pkgInfoSubType === "970, I" || pkgInfoSubType === "967, II" || pkgInfoSubType === "970, II") {
-        result.push({ ok: false, result: "967/970 未勾选堆码，或堆码评估，如果是24年报告请忽略" });
-      }
-      if (pkgInfoSubType === "966, II" || pkgInfoSubType === "969, II") {
-        result.push({ ok: false, result: "966/969 第II部分未勾选堆码，或堆码评估，如果是24年报告请忽略" });
-      }
-    }
     if (stackTest && stackTestEvaluation) {
       result.push({ ok: false, result: "重复勾选堆码和堆码评估" });
     }
-    if (pkgInfoSubType === "965, IB") {
-      if (!stackTest) {
-        result.push({ ok: false, result: "965，IB未勾选堆码" });
+    if (["965, IA", "966, I", "968, IA", "969, I"].includes(pkgInfoSubType)) {
+      if (stackTest || stackTestEvaluation) {
+        result.push({ ok: false, result: `${pkgInfoSubType}不应勾选堆码` });
       }
+    } else {
+      if (["967, I", "970, I", "967, II", "970, II", "966, II", "969, II"].includes(pkgInfoSubType)) {
+        if (!stackTest && !stackTestEvaluation) {
+          result.push({ ok: false, result: `${pkgInfoSubType} 未勾选堆码或堆码评估，如果是24年报告请忽略` });
+        }
+      }
+      if (pkgInfoSubType === "965, IB" || pkgInfoSubType === "968, IB") {
+        if (!stackTest) {
+          result.push({ ok: false, result: `${pkgInfoSubType}未勾选堆码` });
+        }
+        if (stackTestEvaluation) {
+          result.push({ ok: false, result: `${pkgInfoSubType}不应勾选堆码评估` });
+        }
+      }
+    }
+    if (["965, IB", "968, IB", "966, II", "969, II"].includes(pkgInfoSubType)) {
+      if (dropTest) {
+        result.push({ ok: false, result: `${pkgInfoSubType}未勾选跌落` });
+      }
+    } else {
       if (!dropTest) {
-        result.push({ ok: false, result: "965，IB未勾选跌落" });
+        result.push({ ok: false, result: `${pkgInfoSubType}不应勾选跌落` });
       }
-    }
-    if (pkgInfoSubType === "965, IA" || pkgInfoSubType === "966, I" || pkgInfoSubType === "969, I") {
-      if (dropTest) {
-        result.push({ ok: false, result: "965，IA不应勾选跌落" });
-      }
-      if (stackTest) {
-        result.push({ ok: false, result: "965，IA不应勾选堆码" });
-      }
-    }
-    if (pkgInfoSubType === "967, II" || pkgInfoSubType === "969, II") {
-      if (dropTest) {
-        result.push({ ok: false, result: "967，II不应勾选跌落" });
-      }
-    }
-    if (pkgInfoSubType === "966, II" && !dropTest) {
-      result.push({ ok: false, result: "966，II未勾选跌落" });
     }
     return result;
   }
