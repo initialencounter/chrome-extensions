747ca54f6698c2b09b22ad747faec7bd36328e76
initialencounter <2911583893@qq.com>
2024-12-17 23:59:52 +0800

    fix(summary): matchBatteryType

--- ts/src/summary/checkBatteryType.ts
+++ ts/src/summary/checkBatteryType.ts
@@ -11,8 +11,8 @@ const batteryTypeMap = {
 
 
 export function matchBatteryType(summaryBatteryType: string): SekBtyType | '' {
-  if (summaryBatteryType.includes('单芯锂离子电池')) return '504'
-  if (summaryBatteryType.includes('单芯锂金属电池')) return '505'
+  if (summaryBatteryType.includes('单电芯锂离子电池')) return '504'
+  if (summaryBatteryType.includes('单电芯锂金属电池')) return '505'
   if (summaryBatteryType.includes('锂离子电芯')) return '501'
   if (summaryBatteryType.includes('锂金属电池')) return '502'
   if (summaryBatteryType.includes('锂金属电芯')) return '503'
