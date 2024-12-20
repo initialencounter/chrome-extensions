28994ddc2f39e4fefa79ba9764e1bb2ea0d9f435
initialencounter <2911583893@qq.com>
2024-12-20 22:40:13 +0800

    fix: netWeighLimit

--- ts/src/pek/netWeighLimit.ts
+++ ts/src/pek/netWeighLimit.ts
@@ -10,21 +10,21 @@ export function netWeighLimit(netWeight: number, pkgInfoSubType: string): CheckR
   let result: CheckResult[] = []
   // 电池净重
   if (!isNaN(netWeight)) {
-    if (netWeight > 2.5) {
-      if (pkgInfoSubType === '968, IB') {
-        result.push({ ok: false, result: '968，IB 电池净重超过2.5kg' })
-      }
-    } else if (netWeight > 5) {
-      if (pkgInfoSubType === '966, II' || pkgInfoSubType === '967, II' || pkgInfoSubType === '969, II' || pkgInfoSubType === '970, II') {
-        result.push({ ok: false, result: `${pkgInfoSubType} 电池净重超过5kg` })
+    if (netWeight > 35) {
+      if (pkgInfoSubType === '965, IA' || pkgInfoSubType === '966, I' || pkgInfoSubType === '967, I' || pkgInfoSubType === '968, IA' || pkgInfoSubType === '969, I' || pkgInfoSubType === '970, I') {
+        result.push({ ok: false, result: `${pkgInfoSubType} 电池净重超过35kg` })
       }
     } else if (netWeight > 10) {
       if (pkgInfoSubType === '965, IB') {
         result.push({ ok: false, result: `${pkgInfoSubType} 电池净重超过10kg` })
       }
-    } else if (netWeight > 35) {
-      if (pkgInfoSubType === '965, IA' || pkgInfoSubType === '966, I' || pkgInfoSubType === '967, I' || pkgInfoSubType === '968, IA' || pkgInfoSubType === '969, I' || pkgInfoSubType === '970, I') {
-        result.push({ ok: false, result: `${pkgInfoSubType} 电池净重超过35kg` })
+    } else if (netWeight > 5) {
+      if (pkgInfoSubType === '966, II' || pkgInfoSubType === '967, II' || pkgInfoSubType === '969, II' || pkgInfoSubType === '970, II') {
+        result.push({ ok: false, result: `${pkgInfoSubType} 电池净重超过5kg` })
+      }
+    } else if (netWeight > 2.5) {
+      if (pkgInfoSubType === '968, IB') {
+        result.push({ ok: false, result: '968，IB 电池净重超过2.5kg' })
       }
     }
   }
