a5cc68d4d059cd719c2a1a6d5d67a96a28b7e8de
initialencounter <2911583893@qq.com>
2025-01-07 23:22:00 +0800

    fix: 966 miss match battery name

--- ts/src/summary/checkName.ts
+++ ts/src/summary/checkName.ts
@@ -8,48 +8,28 @@ export function checkName(packageType: '0' | '1' | '2', formEName: string, formC
   model = model.trim()
   let formCNameText = ''
   let formENameText = ''
-  let keyWord = ''
   switch (packageType) {
     case '0':
+    case '1':
       formCNameText = formCName.split(model)[0]
       formENameText = formEName.split(model)[0]
       break
-    case '1':
-      keyWord = "包装"
-      break
     case '2':
-      keyWord = "内置"
+      let indexKeyWord = formCName.indexOf('内置')
+      let indexKeyEWord = formEName.indexOf("Containing")
+      let indexModel = formCName.indexOf(model)
+      let indexEModel = formEName.indexOf(model)
+      console.log('indexKeyWord', indexKeyWord)
+      console.log('indexModel', indexModel)
+      if (indexKeyWord < indexModel) {
+        formCNameText = formCName.substring(indexKeyWord + 2, indexModel)
+        formENameText = formEName.substring(indexKeyEWord + 10, indexEModel)
+      }
+      else {
+        formCNameText = formCName.substring(0, indexModel)
+        formENameText = formEName.substring(0, indexModel)
+      }
       break
-  }
-  if (keyWord === '内置') {
-    let indexKeyWord = formCName.indexOf('内置')
-    let indexKeyEWord = formEName.indexOf("Containing")
-    let indexModel = formCName.indexOf(model)
-    let indexEModel = formEName.indexOf(model)
-    console.log('indexKeyWord', indexKeyWord)
-    console.log('indexModel', indexModel)
-    if (indexKeyWord < indexModel) {
-      formCNameText = formCName.substring(indexKeyWord + 2, indexModel)
-      formENameText = formEName.substring(indexKeyEWord + 10, indexEModel)
-    }
-    else {
-      formCNameText = formCName.substring(0, indexModel)
-      formENameText = formEName.substring(0, indexModel)
-    }
-  }
-  else if (keyWord === '包装') {
-    let indexKeyWord = formCName.indexOf('与')
-    let indexKeyEWord = formEName.indexOf('Packed With')
-    let indexModel = formCName.indexOf(model)
-    let indexEModel = formEName.indexOf(model)
-    if (indexKeyWord < indexModel) {
-      formCNameText = formCName.substring(0, indexModel)
-      formENameText = formEName.substring(0, indexEModel)
-    }
-    else {
-      formCNameText = formCName.substring(indexKeyWord + 1, indexModel)
-      formENameText = formEName.substring(indexKeyEWord + 1, indexEModel)
-    }
   }
   formCNameText = formCNameText.trim()
   formENameText = formENameText.trim()
