1de2aa6d5d139f2b1146dbcbce0610267741b706
WWWw <109729945+initialencounter@users.noreply.github.com>
2025-02-17 11:44:14 +0800

    fix: regex match unit

--- ts/src/shared/utils/index.ts
+++ ts/src/shared/utils/index.ts
@@ -8,7 +8,7 @@ export {
 function matchWattHour(projectName: string) {
   const matches = [...projectName.matchAll(/\s(\d+\.?\d+)[Kk]?[Ww][Hh]/g)]
   const results = matches.map((match) => match[1])
-  const rowText = matches.map((match) => match[0])[0]
+  const rowText = matches.map((match) => match[0])[results.length - 1]
   let wattHour = Number(results[results.length - 1])
   if (!results.length) return 0
   if (isNaN(wattHour)) return 0
@@ -28,7 +28,7 @@ function matchVoltage(projectName: string) {
 function matchCapacity(projectName: string) {
   let matches = [...projectName.matchAll(/(\d+\.?\d*)[Mm]?[Aa][Hh]/g)]
   let results = matches.map((match) => match[1])
-  const rowText = matches.map((match) => match[0])[0]
+  const rowText = matches.map((match) => match[0])[results.length - 1]
   let result = Number(results[results.length - 1])
   if (!results.length) return 0
   if (isNaN(result)) return 0
@@ -39,7 +39,7 @@ function matchCapacity(projectName: string) {
 function matchBatteryWeight(describe: string) {
   const matches = [...describe.matchAll(/为(\d+\.?\d*)[Kk]?[g]?/g)]
   const results = matches.map((match) => match[1])
-  const rowText = matches.map((match) => match[0])[0]
+  const rowText = matches.map((match) => match[0])[results.length - 1]
   let weight = Number(results[0])
   if (!results.length) return 0
   if (isNaN(weight)) return 0
