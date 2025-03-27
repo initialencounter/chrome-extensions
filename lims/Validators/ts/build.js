e4c137e69cbb61e419a96ca3960cab2aa2d0fdc4
initialencounter <2911583893@qq.com>
2025-03-28 00:40:57 +0800

    fix: 更新validators构建输出路径，将validators.js文件直接输出到dist目录

--- lims/Validators/ts/build.js
+++ lims/Validators/ts/build.js
@@ -3,7 +3,7 @@ const path = require('path')
 require('esbuild').build({
     entryPoints: [path.resolve(__dirname, 'src/index.ts')], // 入口文件
     bundle: true,
-    outfile: path.resolve(__dirname, '../../src/public/js/validators.js'),
+    outfile: path.resolve(__dirname, '../../dist/js/validators.js'),
     // outfile: 'lib/index.js',
     platform: 'browser', // 设置为浏览器平台
     target: 'esnext', // 根据需要选择目标
