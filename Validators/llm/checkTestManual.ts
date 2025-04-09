01593de69a0cffb7f5b8b3374af50d0ba49ba7b0
initialencounter <2911583893@qq.com>
2025-04-09 16:12:10 +0800

    fix(validators): match TestManual

--- Validators/llm/checkTestManual.ts
+++ Validators/llm/checkTestManual.ts
@@ -12,18 +12,17 @@ type TestManual ="联合国《试验和标准手册》第八修订版第38.3节"
 "联合国《关于危险货物运输的建议书-试验和标准手册》（第4版）38.3节"
 
 export function checkTestManual(rawTestManualLLM: string, rawTestManual: string): CheckResult[] {
-  let testManualLLM = String(rawTestManualLLM).trim()
-  testManualLLM = matchTestManual(testManualLLM)
   let testManual = rawTestManual.trim()
   testManual = matchTestManual(testManual)
-  if (!testManualLLM && !testManual){
+  if (!rawTestManualLLM && !testManual){
     return [{
       ok: false,
       result: '未匹配到UN38.3测试标准，请反馈问题给开发人员'
     }]
   }
-  if (testManualLLM !== testManual) {
-    console.log(testManualLLM, testManual)
+  console.log('raw input', rawTestManualLLM, rawTestManual)
+  console.log('match from docx', testManual)
+  if (rawTestManualLLM !== testManual) {
     return [{
       ok: false,
       result: `UN报告上的测试标准为:${rawTestManualLLM}，概要上的为:${rawTestManual}`
